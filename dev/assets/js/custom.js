e304ba424fc886b42130687c9a1f691b0262d655
Aleksandr <160377@i.ua>
2024-11-09 22:39:32 +0200

    carousel-product

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,18 +1,26 @@
-const btnburger = document.querySelector('.btn-burger');
+const btnburger = document.querySelector('.burger');
 const mobileContainer = document.querySelector('.mobile-container');
 const toggleMobileMenu = () => {
-   mobileContainer.classList.toggle('show');
-   btnburger.classList.toggle('active');
+  mobileContainer.classList.toggle('show');
+  btnburger.classList.toggle('active');
 }
 btnburger.addEventListener('click', toggleMobileMenu);
 
-const swiper = new Swiper('.swiper', {
+const bannerSlider = new Swiper('.banner-slider', {
   slidesPerView: 2,
   loop: true,
-  speed: 700,
-  spaceBetween:20,
+  speed: 1000,
+  spaceBetween: 20,
   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
- });
+});
+
+const carouselWeekly = new Swiper('.carousel-weekly', {
+  slidesPerView: 3,
+  loop: true,
+  speed: 1000,
+  spaceBetween: 20,
+  centeredSlides: true,
+});
